8b9705e933b6624748886b21b5e632d15673a11f
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2023-10-12 17:45:30 +0200

    ad

---
+++ script.js
@@ -0,0 +1,36 @@
+function validarFormulario() {
+    var dni = document.getElementById('dni').value;
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var telefono = document.getElementById('telefono').value;
+    var email = document.getElementById('email').value;
+
+    // Validar DNI
+    var dniRegex = /^[0-9]{8}[A-Za-z]$/;
+    if (!dniRegex.test(dni)) {
+        alert('Formato de DNI inválido');
+        return false;
+    }
+
+    // Validar nombre y apellido
+    if (nombre == '' || apellido == '') {
+        alert('Por favor, ingrese su nombre y apellido');
+        return false;
+    }
+
+    // Validar teléfono
+    var telRegex = /^[0-9]{9}$/;
+    if (!telRegex.test(telefono)) {
+        alert('Formato de teléfono inválido');
+        return false;
+    }
+
+    // Validar correo electrónico
+    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+        alert('Formato de correo electrónico inválido');
+        return false;
+    }
+
+    alert('Formulario enviado correctamente');
+}
